7389949bb3181e70443ac489d1b15d11983c063f
bistgustavo <bistgaurav024@gmail.com>
2025-04-18 22:20:10 +0545

    commit for comment in asynchandler

--- Backend/src/utils/asynchandler.js
+++ Backend/src/utils/asynchandler.js
@@ -1,3 +1,5 @@
+//This code defines an asyncHandler utility function that wraps around Express route handlers to simplify error //////handling for async/await operations. 
+
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
